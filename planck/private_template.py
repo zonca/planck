855dfc991979093bb0bb097acd8b03285b7f0538
Andrea Zonca <code@andreazonca.com>
2010-11-01 10:38:40 -0700

    template for the private.py file not included in the distribution

---
+++ planck/private_template.py
@@ -0,0 +1,14 @@
+#add all the paths and rename to private.py
+LFI_rimo = ''
+HFI_rimo = ''
+instrument_db = ''
+savfilename = ''
+AHF_limits = ''
+limitsfile =''
+siam = ''
+
+mKRJ_2_mKcmb = {
+}
+
+mkCMB_2_MJy_sr = {
+}
