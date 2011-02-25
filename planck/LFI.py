fixed psi angle computation

planck/LFI.py
@@ -39,6 +39,9 @@ class LFIChannel(Planck.Channel):
     def wn(self): 
         return self.inst.instrument_db(self).field('NET_KCMB')
 
+    def get_instrument_db_field(self, field): 
+        return self.inst.instrument_db(self).field(field)
+
     def __getitem__(self, n):
         return self.d[n]
 
@@ -80,7 +83,7 @@ class LFI(Planck.Instrument):
     def instrument_db(self,ch):
         if not hasattr(self,'_instrument_db'):
             import pyfits
-            self._instrument_db = pyfits.open(private.instrument_db)[1].data
+            self._instrument_db = pyfits.open(private.instrument_db,ignore_missing_end=True)[1].data
         det_index, = np.where(self._instrument_db.field('RADIOMETER').rfind(ch.tag)== 0)
         return self._instrument_db[det_index]
 

planck/pointing.py
@@ -116,7 +116,11 @@ class Pointing(object):
         vec = self.get(rad)
         theta, phi = vec2ang(vec)
         psi = self.compute_psi(theta, phi, rad)
-        return vec2pix(nside, vec[:,0], vec[:,1], vec[:,2], nest), np.cos(2*psi), np.sin(2*psi)
+        #return vec2pix(nside, vec[:,0], vec[:,1], vec[:,2], nest), np.cos(2*psi), np.sin(2*psi)
+        spsi = np.sin(psi)
+        cpsi = np.cos(psi)
+        cf = 1./(cpsi**2 + spsi**2)
+        return vec2pix(nside, vec[:,0], vec[:,1], vec[:,2], nest), (cpsi**2 - spsi**2)*cf, 2*cpsi*spsi*cf
 
     def get_pix(self, rad, nside=1024, nest=True):
         from healpy import vec2pix

planck/pointingtools.py
@@ -55,9 +55,9 @@ class SiamAngles(object):
 
     def get(self, ch):
         mat_spin2boresight=mat3.rotation(np.pi/2-self.SPIN2BORESIGHT/180.*np.pi,vec3(0,1,0))
-        theta = np.radians(ch.rimo['THETA_UV'])
-        phi = np.radians(ch.rimo['PHI_UV'])
-        psi = np.radians(ch.rimo['PSI_UV'])
+        theta = np.radians(ch.get_instrument_db_field('THETA_UV'))
+        phi = np.radians(ch.get_instrument_db_field('PHI_UV'))
+        psi = np.radians(ch.get_instrument_db_field('PSI_UV')+ch.get_instrument_db_field('PSI_POL'))
         mat_theta_phi = mat3.rotation(theta,vec3(-math.sin(phi),math.cos(phi),0))
         mat_psi = mat3.rotation(psi,vec3(0,0,1))
         # detector points to X axis