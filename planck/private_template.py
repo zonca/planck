a846b470fbace11e4a8192dbf2e1f10b759a398d
Andrea Zonca <code@andreazonca.com>
2011-06-07 11:41:10 -0700

    template updated

--- planck/private_template.py
+++ planck/private_template.py
@@ -1,11 +1,24 @@
+import numpy as np
 #add all the paths and rename to private.py
+database = ''
+exchangefolder = {'LFI':'',
+                  'HFI':''}
 LFI_rimo = ''
 HFI_rimo = ''
 instrument_db = ''
 savfilename = ''
 AHF_limits = ''
-limitsfile =''
 siam = ''
+#siam = '/project/projectdirs/planck/data/mission/SIAM/siam_IMO244_100720.m3'
+PIX2ODPATH = ''
+HFI_calibfile = ''
+
+WOBBLE = { 'psi1_ref' : 0.,
+           'psi2_ref' : 0.,
+           'psi2_file' : '',
+           'sun_file' : '',
+           'planck_file' : '',
+         }
 
 mKRJ_2_mKcmb = {
 }
