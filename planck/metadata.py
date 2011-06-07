74304da6acf1a464843072f0e41cdc993899f5f6
Andrea Zonca <code@andreazonca.com>
2011-06-07 10:10:32 -0700

    centralized module for data selection

--- planck/metadata.py
+++ planck/metadata.py
@@ -42,34 +42,35 @@ class DataSelector(object):
 
     def get_AHF(self):
         return [self.get_one_AHF(obt_range) for obt_range in self.obt_ranges]
-        
 
-def ods_btw_obt(freq, obtrange):
-    '''List of operational days in the obtrange supplied'''
-    assert obtrange[1]>=obtrange[0]
-    conn = sqlite3.connect(default_config()['database'])
-    c = conn.cursor()
-    query = c.execute('select od from efdd_od_ranges where freq=? and endOBT>=? and startOBT<=?',(freq, obtrange[0]*2**16, obtrange[1]*2**16) )
-    ods = [q[0] for q in query]
-    c.close()
-    return ods
+    @classmethod
+    def get_ODs(cls, obtrange):
+        """List of ODs within the obt range provided"""
+        assert obtrange[1]>=obtrange[0]
+        conn = sqlite3.connect(self.config['database'])
+        c = conn.cursor()
+        query = c.execute('select od from efdd_od_ranges where freq=? and endOBT>=? and startOBT<=?',(freq, obtrange[0]*2**16, obtrange[1]*2**16) )
+        ods = [q[0] for q in query]
+        c.close()
+        return ods
 
-def latest_exchange(freq, od, exchangefolder = None, type = 'R'):
-    '''Returns the latest version of an exchange format file'''
-    if exchangefolder is None:
-        exchangefolder = default_config()['exchangefolder'][freq2inst(freq)]
-    if glob.glob(exchangefolder + '/*.fits'):
-        od = 0
-    if type == 'K':
-        type = ''
-    pattern = '/*%03d-%04d-%s*.fits' % (freq, od, type)
-    if od:
-        pattern = ('/%04d' % od) + pattern
-    l.debug('Exchange format: %s' % (exchangefolder + pattern))
-    allversions = glob.glob(exchangefolder + pattern)
-    if not allversions:
-        error_message = 'Cannot find file from pattern: %s' % (exchangefolder +         pattern)
-        l.error(error_message)
-        raise exceptions.IOError(error_message)
+    @classmethod
+    def latest_exchange(cls, freq, od, exchangefolder = None, type = 'R'):
+        """Returns the latest version of an exchange format file"""
+        if exchangefolder is None:
+            exchangefolder = cls.config['exchangefolder'][freq2inst(freq)]
+        if glob.glob(exchangefolder + '/*.fits'):
+            od = 0
+        if type == 'K':
+            type = ''
+        pattern = '/*%03d-%04d-%s*.fits' % (freq, od, type)
+        if od:
+            pattern = ('/%04d' % od) + pattern
+        l.debug('Exchange format: %s' % (exchangefolder + pattern))
+        allversions = glob.glob(exchangefolder + pattern)
+        if not allversions:
+            error_message = 'Cannot find file from pattern: %s' % (exchangefolder +         pattern)
+            l.error(error_message)
+            raise exceptions.IOError(error_message)
 
-    return max(allversions, key = lambda x: int(x[-13:-5]))
+        return max(allversions, key = lambda x: int(x[-13:-5]))
